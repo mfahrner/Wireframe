fc54e01169e01323c72c52543208c71c499f12fa
michael fahrner <Mike Fahrner>
2016-09-21 21:35:41 -0400

    first commit

---
+++ src/main/java/com/theironyard/charlotte/WireframeApplication.java
@@ -0,0 +1,12 @@
+package com.theironyard.charlotte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WireframeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WireframeApplication.class, args);
+	}
+}
---
+++ src/main/java/com/theironyard/charlotte/WireframeController.java
@@ -0,0 +1,11 @@
+package com.theironyard.charlotte;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by mfahrner on 9/21/16.
+ */
+@Controller
+public class WireframeController {
+
+}
